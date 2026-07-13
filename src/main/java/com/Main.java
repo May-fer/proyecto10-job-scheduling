a27b8bfc64928c83f7e2e3469cd9f5068a600ddb
Antony2512 <antonychimbo372@gmail.com>
2026-07-13 18:45:47 -0500

    Create Main class for JavaFX application
    Initial implementation of the JavaFX application for job scheduling.

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,36 @@
+package com.epn.proyecto10;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/main-view.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1200, 700);
+
+        scene.getStylesheets().add(
+                getClass().getResource("/css/styles.css").toExternalForm()
+        );
+
+        stage.setTitle("Proyecto 10 - Job Scheduling");
+        stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
