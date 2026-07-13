182cfd7578c5336ea98c868d1b8d93bb5f014d51
Antony2512 <antonychimbo372@gmail.com>
2026-07-13 18:34:05 -0500

    Clases resultado y Task

---
+++ src/main/java/com/modelo/Resultado.java
@@ -0,0 +1,67 @@
+
+package com.epn.proyecto10.model;
+
+import java.util.List;
+
+public class Resultado {
+
+    private List<Machine> maquinas;
+    private int valorOptimo;
+    private long tiempoEjecucion;
+    private long comparaciones;
+
+    public Resultado() {
+    }
+
+    public Resultado(List<Machine> maquinas,
+                     int valorOptimo,
+                     long tiempoEjecucion,
+                     long comparaciones) {
+
+        this.maquinas = maquinas;
+        this.valorOptimo = valorOptimo;
+        this.tiempoEjecucion = tiempoEjecucion;
+        this.comparaciones = comparaciones;
+    }
+
+    public List<Machine> getMaquinas() {
+        return maquinas;
+    }
+
+    public void setMaquinas(List<Machine> maquinas) {
+        this.maquinas = maquinas;
+    }
+
+    public int getValorOptimo() {
+        return valorOptimo;
+    }
+
+    public void setValorOptimo(int valorOptimo) {
+        this.valorOptimo = valorOptimo;
+    }
+
+    public long getTiempoEjecucion() {
+        return tiempoEjecucion;
+    }
+
+    public void setTiempoEjecucion(long tiempoEjecucion) {
+        this.tiempoEjecucion = tiempoEjecucion;
+    }
+
+    public long getComparaciones() {
+        return comparaciones;
+    }
+
+    public void setComparaciones(long comparaciones) {
+        this.comparaciones = comparaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado{" +
+                "valorOptimo=" + valorOptimo +
+                ", tiempoEjecucion=" + tiempoEjecucion +
+                ", comparaciones=" + comparaciones +
+                '}';
+    }
+}
---
+++ src/main/java/com/modelo/Task.java
@@ -0,0 +1,84 @@
+
+
+package com.epn.proyecto10.model;
+
+public class Task {
+
+    private int id;
+    private int inicio;
+    private int fin;
+    private int peso;
+    private Machine maquina;
+
+    public Task() {
+    }
+
+    public Task(int id, int inicio, int fin, int peso) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.peso = peso;
+    }
+
+    public Task(int id, int inicio, int fin, int peso, Machine maquina) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.peso = peso;
+        this.maquina = maquina;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public void setFin(int fin) {
+        this.fin = fin;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public Machine getMaquina() {
+        return maquina;
+    }
+
+    public void setMaquina(Machine maquina) {
+        this.maquina = maquina;
+    }
+
+    public int getDuracion() {
+        return fin - inicio;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", inicio=" + inicio +
+                ", fin=" + fin +
+                ", peso=" + peso +
+                '}';
+    }
+}
