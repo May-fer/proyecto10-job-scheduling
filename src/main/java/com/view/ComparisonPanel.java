276c5edd12d32873b3b6b291da4b5dbcffafd576
Antony2512 <antonychimbo372@gmail.com>
2026-07-13 18:35:18 -0500

    Clases comparison y Ganttcharview

---
+++ src/main/java/com/view/ComparisonPanel.java
@@ -0,0 +1,68 @@
+package com.epn.proyecto10.view;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class ComparisonPanel extends GridPane {
+
+    private final Label dpValor;
+    private final Label greedyValor;
+    private final Label bbValor;
+
+    private final Label dpTiempo;
+    private final Label greedyTiempo;
+    private final Label bbTiempo;
+
+    public ComparisonPanel() {
+
+        setPadding(new Insets(15));
+        setHgap(20);
+        setVgap(10);
+
+        add(new Label("Algoritmo"),0,0);
+        add(new Label("Valor"),1,0);
+        add(new Label("Tiempo (ms)"),2,0);
+
+        add(new Label("Dynamic Programming"),0,1);
+        add(new Label("Greedy"),0,2);
+        add(new Label("Branch & Bound"),0,3);
+
+        dpValor = new Label("-");
+        greedyValor = new Label("-");
+        bbValor = new Label("-");
+
+        dpTiempo = new Label("-");
+        greedyTiempo = new Label("-");
+        bbTiempo = new Label("-");
+
+        add(dpValor,1,1);
+        add(greedyValor,1,2);
+        add(bbValor,1,3);
+
+        add(dpTiempo,2,1);
+        add(greedyTiempo,2,2);
+        add(bbTiempo,2,3);
+    }
+
+    public void actualizarDP(int valor,long tiempo){
+
+        dpValor.setText(String.valueOf(valor));
+        dpTiempo.setText(tiempo + " ms");
+
+    }
+
+    public void actualizarGreedy(int valor,long tiempo){
+
+        greedyValor.setText(String.valueOf(valor));
+        greedyTiempo.setText(tiempo + " ms");
+
+    }
+
+    public void actualizarBranchBound(int valor,long tiempo){
+
+        bbValor.setText(String.valueOf(valor));
+        bbTiempo.setText(tiempo + " ms");
+
+    }
+}
---
+++ src/main/java/com/view/GanttChartView.java
@@ -0,0 +1,72 @@
+package com.epn.proyecto10.view;
+
+import com.epn.proyecto10.model.Machine;
+import com.epn.proyecto10.model.Task;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.List;
+
+public class GanttChartView extends VBox {
+
+    private final Pane panel;
+
+    public GanttChartView() {
+
+        setSpacing(10);
+        setPadding(new Insets(10));
+
+        Label titulo = new Label("Diagrama de Gantt");
+
+        panel = new Pane();
+        panel.setPrefSize(900, 400);
+
+        getChildren().addAll(titulo, panel);
+    }
+
+    public void mostrar(List<Machine> maquinas) {
+
+        panel.getChildren().clear();
+
+        double altoFila = 60;
+        double escala = 40;
+
+        for (int i = 0; i < maquinas.size(); i++) {
+
+            Machine maquina = maquinas.get(i);
+
+            Label nombre = new Label("Máquina " + maquina.getId());
+            nombre.setLayoutX(10);
+            nombre.setLayoutY(i * altoFila + 20);
+
+            panel.getChildren().add(nombre);
+
+            for (Task tarea : maquina.getTareas()) {
+
+                Rectangle rect = new Rectangle();
+
+                rect.setX(120 + tarea.getInicio() * escala);
+                rect.setY(i * altoFila + 10);
+                rect.setWidth(tarea.getDuracion() * escala);
+                rect.setHeight(35);
+
+                rect.setFill(Color.LIGHTBLUE);
+                rect.setStroke(Color.BLACK);
+
+                Label id = new Label("T" + tarea.getId());
+                id.setLayoutX(rect.getX() + 10);
+                id.setLayoutY(rect.getY() + 8);
+
+                panel.getChildren().addAll(rect, id);
+            }
+        }
+    }
+
+    public Pane getPanel() {
+        return panel;
+    }
+}
