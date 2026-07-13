d54b56ba030f44f2e273a0f57db7ba99767a5263
Antony2512 <antonychimbo372@gmail.com>
2026-07-13 18:33:10 -0500

    Clase machine

---
+++ src/main/java/com/modelo/Machine.java
@@ -0,0 +1,58 @@
+
+package com.epn.proyecto10.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Machine {
+
+    private int id;
+    private List<Task> tareas;
+
+    public Machine() {
+        tareas = new ArrayList<>();
+    }
+
+    public Machine(int id) {
+        this.id = id;
+        tareas = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Task> getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(List<Task> tareas) {
+        this.tareas = tareas;
+    }
+
+    public void agregarTarea(Task tarea) {
+        tareas.add(tarea);
+    }
+
+    public int getTiempoTotal() {
+        int total = 0;
+
+        for (Task tarea : tareas) {
+            total += tarea.getDuracion();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Machine{" +
+                "id=" + id +
+                ", tareas=" + tareas.size() +
+                '}';
+    }
+}
